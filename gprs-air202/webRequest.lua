d5b4ab36ea17f0ea61e058c456680510db76a610
yangbo1979 <gyangbo@gmail.com>
2018-01-26 16:49:46 +0800

    remove iccid display

--- gprs-air202/webRequest.lua
+++ gprs-air202/webRequest.lua
@@ -290,7 +290,7 @@ function getIccid()
 		end
 		PostData = PostData.."}"
 		data = "POST /api/v1/gateway/updatebysn/"..misc.getimei().." HTTP/1.1\r\nHost: www.lewei50.com\r\nContent-Length: " .. string.len(PostData) .. "\r\n\r\n"..PostData .. "\r\n"
-		lcd.setInfo(iccid)
+		--lcd.setInfo(iccid)
 		snd(data)
 		sys.timer_stop(getIccid)
 	end
